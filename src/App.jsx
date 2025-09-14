66e7787a1341af38e65f0f3e4d6753fef3e5b402
Vikas Gupta <148651645+vikastech15@users.noreply.github.com>
2025-09-14 23:27:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,80 @@
+// import './App.css'
+// import AddBook from './pages/AddBook'
+// import ShowBook from './pages/showBook'
+// import Home from './pages/Home'
+// import Browse from './pages/Browse'
+// import Login from './pages/Login'
+// import Signup from './pages/Signup'
+// import Community from './pages/Community'
+// // import ScrollToTop from "./components/ScrollToTop";
+
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import Profile from './pages/Profile'
+// import CheckoutPage from './pages/Checkout'
+// import CartPage from './pages/Cart'
+// import AdminDashboard from './pages/Admin'
+// function App() {
+
+//   const router = createBrowserRouter([
+//       {
+//         path: "/",
+//         element: <Home/>,
+//       },
+//       {
+//         path: "/Community",
+//         element: <Community/>,
+//       },
+//       {
+//         path: "/ShowBooks/:id",
+//         element: <ShowBook/>,
+
+//       },
+//       {
+//         path: "/AddBook",
+//         element: <AddBook/>,
+//       },
+//       {
+//         path: "/Browse",
+//         element: <Browse/>,
+//       },
+//       {
+//         path: "Login",
+//         element: <Login/>,
+//       },
+//       {
+//          path: "Signup",
+//           element: <Signup/>,
+//       },
+//       {
+//         path: "Profile",
+//         element: <Profile/>
+//       },
+//       {
+//         path: "Checkout",
+//         element: <CheckoutPage/>
+//       },
+//       {
+//         path: "Cart",
+//         element: <CartPage/>
+//       },
+//       {
+//         path: "Admin",
+//         element: <AdminDashboard/>
+//       },
+//     ])
+
+//   return (
+//     <>
+//                {/* <ScrollToTop /> */}
+//        <RouterProvider router={router} />
+
+//       </>
+//   )
+// }
+
+// export default App
+
+
 import './App.css'
 import AddBook from './pages/AddBook'
 import ShowBook from './pages/showBook'
@@ -6,70 +83,45 @@ import Browse from './pages/Browse'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Community from './pages/Community'
-// import ScrollToTop from "./components/ScrollToTop";
+import ScrollToTop from "./components/ScrollToTop";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 import Profile from './pages/Profile'
 import CheckoutPage from './pages/Checkout'
 import CartPage from './pages/Cart'
 import AdminDashboard from './pages/Admin'
-function App() {
 
-  const router = createBrowserRouter([
-      {
-        path: "/",
-        element: <Home/>,
-      },
-      {
-        path: "/Community",
-        element: <Community/>,
-      },
-      {
-        path: "/ShowBooks/:id",
-        element: <ShowBook/>,
-
-      },
-      {
-        path: "/AddBook",
-        element: <AddBook/>,
-      },
-      {
-        path: "/Browse",
-        element: <Browse/>,
-      },
-      {
-        path: "Login",
-        element: <Login/>,
-      },
-      {
-         path: "Signup",
-          element: <Signup/>,
-      },
-      {
-        path: "Profile",
-        element: <Profile/>
-      },
-      {
-        path: "Checkout",
-        element: <CheckoutPage/>
-      },
-      {
-        path: "Cart",
-        element: <CartPage/>
-      },
-      {
-        path: "Admin",
-        element: <AdminDashboard/>
-      },
-    ])
-
+function Layout() {
   return (
     <>
-               {/* <ScrollToTop /> */}
-       <RouterProvider router={router} />
-
-      </>
-  )
+      <ScrollToTop />
+      <Outlet />
+    </>
+  );
 }
 
-export default App
+function App() {
+  const router = createBrowserRouter([
+    {
+      element: <Layout />,   // 👈 wrap all routes in Layout
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/Community", element: <Community /> },
+        { path: "/ShowBooks/:id", element: <ShowBook /> },
+        { path: "/AddBook", element: <AddBook /> },
+        { path: "/Browse", element: <Browse /> },
+        { path: "/Login", element: <Login /> },
+        { path: "/Signup", element: <Signup /> },
+        { path: "/Profile", element: <Profile /> },
+        { path: "/Checkout", element: <CheckoutPage /> },
+        { path: "/Cart", element: <CartPage /> },
+        { path: "/Admin", element: <AdminDashboard /> },
+      ]
+    }
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
+
